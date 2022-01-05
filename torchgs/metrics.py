09002f54dc0965861dc6bbb45ba977746930a786
Daniel Ik <codingeinstein@gmail.com>
2022-01-05 14:21:44 +0100

    added metrics.py

---
+++ torchgs/metrics.py
@@ -0,0 +1,20 @@
+import torch
+
+
+class Metric:
+    def __init__(self):
+        pass
+
+
+class Loss(Metric):
+    def __init__(self):
+        super().__init__()
+
+class Accuracy(Metric):
+    def __init__(self):
+        super().__init__()
+
+
+class F1(Metric):
+    def __init__(self):
+        super().__init__()
