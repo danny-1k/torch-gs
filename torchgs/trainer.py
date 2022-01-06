d2ad3a61f017745490f74bf83ef6de5c105e4ba3
Daniel Ik <codingeinstein@gmail.com>
2022-01-06 14:48:13 +0100

    updated trainer.py and fixed bugs

--- torchgs/trainer.py
+++ torchgs/trainer.py
@@ -2,20 +2,31 @@ import torch
 from .metrics import Metric
 
 class Trainer:
-    def __init__(self,net,lossfn,optimizer,lrschedulers=None,losshandler=None,metric=None):
+    def __init__(self,params):
         self.device  = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.net = net
-        self.lossfn = lossfn
-        self.optimizer = optimizer
-        self.losshandler = losshandler
-        self.lrschedulers = lrschedulers if lrschedulers!=None else []
+        self._set_params(params)
+
+
+    def _set_params(self,params={}):
+        assert params.get('net') !=None , 'params["net"] must not be None.'
+        assert params.get('lossfn')!=None or params.get('criterion')!=None, 'params["lossfn"] or params["criterion"] must not be None.'
+        assert params.get('optimizer')!=None,'params["optimizer"] must not be None.'
+        
+
+        assert isinstance(params.get('net'),torch.nn.Module), 'params["net"] must be an instance of the `torch.nn.Module` class.'
+        assert isinstance(params.get('lossfn'),torch.nn.Module) or isinstance(params.get('criterion'),torch.nn.Module),'params["lossfn"] or params["criterion"] must be an instance of the `torch.nn.Module` class.'
+        assert isinstance(params.get('optimizer'),torch.optim.Optimizer),'params["optimizer"] must be an instance of the `torch.optim.Optimizer` class.'
+        assert isinstance(params.get('metric'),Metric) or params.get('metric')==None, 'params["metric"] must an instance of the `metrics.Metric` class.'
+        
+
+        self.net = params.get('net')
+        self.lossfn = params.get('lossfn') or params.get('criterion')
+        self.optimizer = params.get('optimizer')
+        self.lrschedulers = ([params.get('lrschedulers')] if type(params.get('lrschedulers')) !=list else  params.get('lrschedulers') )if params.get('lrschedulers')!=None else  []
+        self.metric = params.get('metric')
         self.performance = {}
-        self.metric = metric
 
         self.net.to(self.device)
-
-        if self.metric !=None:
-            assert isinstance(metric,Metric), '`metric` must be a class inherited from `metrics.Metric`'
 
 
     def train_on(self,trainloader,testloader=None,epochs=1):
@@ -40,8 +51,8 @@ class Trainer:
                 loss.backward()
                 self.optimizer.step()
 
-                for sched in self.lrschedulers:
-                    sched.step()
+            for sched in self.lrschedulers:
+                sched.step(loss)
 
             self.performance['train'][epoch+1] = loss.item()
 
