fb69fea4b48e57f3a1573b0784d75390897f3f8c
Daniel Ik <codingeinstein@gmail.com>
2022-01-06 17:01:00 +0100

    updated optimizers.py

--- torchgs/optimizers.py
+++ torchgs/optimizers.py
@@ -1,5 +1,24 @@
 import torch
 
 class Optimizer:
-    def __init__(self,optimizer):
-        pass
+    def __init__(self,optimizer,params):
+        """
+        Optimizer class
+
+        Args:
+            optimizer (torch.optim.Optimizer): optimizer
+            params (dict): dictionary of parameters
+        """        
+        self.optimizer = optimizer
+        self.params = params
+        self._set_params(self,self.params)
+
+    def _set_params(self,params):
+        """
+        Method to change some attributes of the optimizer
+
+        Args:
+            params (dict): dict of attributes and values
+        """        
+        for param in params:
+            self.optimizer.__setattr__(param,self.params[param])
