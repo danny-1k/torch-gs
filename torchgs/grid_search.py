65eb5a816b11e95ef6f2bc091bd8511190ce0784
Daniel Ik <codingeinstein@gmail.com>
2022-01-07 20:09:15 +0100

    added fit_once method

--- torchgs/grid_search.py
+++ torchgs/grid_search.py
@@ -1,10 +1,11 @@
 import copy
 import torch
+import torch.nn as nn
 from torch.optim import lr_scheduler
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset,DataLoader
 from .trainer import Trainer
 
-from .optimizers import Optimizer
+from .optimizers import Optimizer,LRscheduler
 from .metrics import Loss
 
 class GridSearch:
@@ -77,12 +78,36 @@ class GridSearch:
 
         trainer_params = self.search_space.get('trainer')
         optimizer_params = self.search_space.get('optimizer')
-        lrscheduler_params = self.search_space.get('lrcheduler')
+        lrschedulers_params = self.search_space.get('lrchedulers')
 
-    def fit_once(self,net,params):
+    def fit_once(self,net,params,train,test):
+        """
+        Fit the net with a current set of parameters, a
+        train dataset and an optional test dataset
+
+        Args:
+            net (nn.Module): network 
+            params (dict): parameter dict
+            train (Dataset): train dataset
+            test (Dataset,optional): test dataset
+
+        Returns:
+            dict: performance over the epochs trained on
+        """        
         trainer_params = params.get('trainer')
         optimizer_params = params.get('optimizer')
-        lrscheduler_params = params.get('lrcheduler')
+        lrschedulers_params = params.get('lrchedulers')
+        train_loader_params = params.get('train_loader')
+        test_loader_params = params.get('test_loader')
+
+        if type(trainer_params.get('lrchedulers')) == list:
+            lrschedulers = []
+            
+            for idx,sched in enumerate(trainer_params.get('lrchedulers')):
+                lrschedulers.append(LRscheduler(sched,lrschedulers_params[idx]))
+
+        else:
+            lrschedulers = None
 
 
         optimizer = Optimizer(trainer_params.get('optimizer'),optimizer_params)
@@ -92,10 +117,17 @@ class GridSearch:
                 'net':net,
                 'lossfn':trainer_params.get('lossfn'),
                 'optimizer':optimizer,
-                'lrschedulers':trainer_params.get('lrcheduler'),
+                'lrschedulers':lrschedulers,
                 'metric':trainer_params.get('metric'),
             }
         )
 
+        train_loader = DataLoader(train,*train_loader_params)
+        test_loader = DataLoader(test,*test_loader_params)
 
 
+        return trainer.train_on(
+            trainloader=train_loader,
+            testloader=test_loader,
+            epochs=trainer_params.get('epochs') or 1,
+        )
