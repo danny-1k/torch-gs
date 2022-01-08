7122f81dbe816feb590b440006ba932315990293
Daniel Ik <codingeinstein@gmail.com>
2022-01-08 12:36:53 +0100

    get stats of performance over time

--- torchgs/metrics.py
+++ torchgs/metrics.py
@@ -1,13 +1,27 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 
 
 class Metric:
     def __init__(self):
         pass
 
-    def evaluate(self, net, loader):
+    def evaluate(self, net: nn.Module, loader: DataLoader):
         return self._evaluate_func(net, loader)
+
+    def get_performance(self, performance: dict):
+        vals = torch.Tensor(list(performance.values()))
+
+        min_val = torch.min(vals)
+        max_val = torch.man(vals)
+        mean_val = torch.mean(vals)
+        std_val = torch.std(vals)
+
+        return {'min': min_val,
+                'max': max_val,
+                'mean': mean_val,
+                'std': std_val}
 
 
 class Loss(Metric):
@@ -16,8 +30,8 @@ class Loss(Metric):
         self.lossfn = lossfn
         self.maximize = False
 
-    def _evaluate_func(self, net, loader):
-        with torch.no_grad:
+    def _evaluate_func(self, net: nn.Module, loader: DataLoader):
+        with torch.no_grad():
             mean_loss = []
 
             for x, y in loader:
@@ -34,13 +48,15 @@ class Accuracy(Metric):
         super().__init__()
         self.maximize = True
 
-    def _evaluate_func(self, net, loader):
-        with torch.no_grad:
+    def _evaluate_func(self, net: nn.Module, loader: DataLoader):
+        with torch.no_grad():
             accuracy = 0
             for x, y in loader:
                 pred = net(x)
                 accuracy += sum(pred.argmax(dim=1) == y)
             accuracy = accuracy/len(loader.dataset)
+
+        return accuracy
 
 
 class Recall(Metric):
@@ -48,14 +64,26 @@ class Recall(Metric):
         super().__init__()
         self.maximize = True
 
+    def _evaluate_func(self, net: nn.Module, loader: DataLoader):
+        with torch.no_grad():
+            pass
+
 
 class Precision(Metric):
     def __init__(self):
         super().__init__()
         self.maximize = True
 
+    def _evaluate_func(self, net: nn.Module, loader: DataLoader):
+        with torch.no_grad():
+            pass
+
 
 class F1(Metric):
     def __init__(self):
         super().__init__()
         self.maximize = True
+
+    def _evaluate_func(self, net: nn.Module, loader: DataLoader):
+        with torch.no_grad():
+            pass
