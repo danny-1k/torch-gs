fd4e67f5c3bdccfc139f0c4411b55784680d9067
Daniel Ik <codingeinstein@gmail.com>
2022-01-08 15:12:32 +0100

    updated __init__.py

--- torchgs/__init__.py
+++ torchgs/__init__.py
@@ -6,9 +6,6 @@ except:
 
 from .trainer import Trainer
 from .grid_search import GridSearch
-from .metrics import Loss,Accuracy,Recall,F1,Precision
-from .optimizers import Optimizer,LRscheduler
+from . import metrics,optimizers
     
-__all__ = ['Trainer','GridSearch','Loss',
-            'Accuracy','Recall','F1','Precision',
-            'Optimizer','LRscheduler']
+__all__ = ['Trainer','GridSearch','metrics','optimizers']
